3b6aaca75fd6c1d06b4ab741fc82e309bf26ada6
ndmanh1000 <157332604+ndmanh1000@users.noreply.github.com>
2025-11-24 08:42:21 +0700

    cm

--- next-landing-vpn-main/next-landing-vpn-main/next.config.js
+++ next-landing-vpn-main/next-landing-vpn-main/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    // Tắt SWC minify để sử dụng Babel với plugin inline-react-svg
+    swcMinify: false,
 }
 
 module.exports = nextConfig
